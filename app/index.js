d74a0bec6a75e23f46c0c9445dd00dc3ee7a9b19
SoLYX1349 <saul77023@gmail.com>
2024-03-30 19:20:10 -0600

    Se termino la presentacion de la pagina principal

--- app/index.js
+++ app/index.js
@@ -1,4 +1,5 @@
-let jsonBicicletas;
+let jsonBicicletas
+let sessonActive = false
 
 // Desconvertir el objeto .json
 function getInformationProducts() {
@@ -9,18 +10,19 @@ function getInformationProducts() {
 // Da formato a la variable
 function addInformation(nombre, tipo, rango, marca, color, tamano, precio, descripcion, url_img) {
 
-// Arreglar este codigo en el CSS
+    // Arreglar este codigo en el CSS
     return "<div class=\"cont-slots-pr\">" +
         "<div class=\"cont-slots-pr_e1\"><img src=\"" + url_img + "\"></div>" +
         "<div class=\"cont-slots-pr_e2\">" +
-        "<div><p>Nombre: </p><p>" + nombre + "</p></div><div>" +
+        "<div><p>Nombre </p><p>" + nombre + "</p></div><div>" +
         "<p>Tipo: </p><p>" + tipo + "</p></div>" +
         "<div><p>Rango edad: </p><p>" + rango + "</p></div>" +
-        "<div><p>Marca: </p><p>" + marca + "</p></div>" +
-        "<div><p>Color: </p><p>" + color + "</p></div>" +
-        "<div><p>Tamaño rueda: </p><p>" + tamano + "</p><p>IN</p></div>" +
-        "<div><p>Precio:</p><p> $" + precio + " </p><p>USD</p><div><button id=\"button-slots-pr-1\"></button><button id=\"button-slots-pr-2\"></button></div></div>" +
-        "<div><p>Descripcion: </p><p>" + descripcion + "</p></div></div></div>";
+        "<div><p>Marca </p><p>" + marca + "</p></div>" +
+        "<div><p>Color </p><p>" + color + "</p></div>" +
+        "<div><p>Tamaño rueda </p><p>" + tamano + " IN</p></div>" +
+        "<div><p>Precio</p><p> $" + precio + " USD</p></div>" +
+        "<div id=\"cont_button_compra\"><button id=\"button-slots-pr-1\">+</button><button id=\"button-slots-pr-2\">-</button></div>" +
+        "<div><p>Descripcion </p><p>" + descripcion + "</p></div></div></div>";
 }
 // Agreaga la informacion a las variables
 function start() {
